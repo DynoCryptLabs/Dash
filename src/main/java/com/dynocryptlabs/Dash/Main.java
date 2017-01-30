de0df9c18b2415749e57cc9ea8731beea8629bba
Rohan Panchal <rohan@rohanpanchal.com>
2017-01-29 21:59:49 -0600

    Created Main class with main function.

---
+++ src/main/java/com/dynocryptlabs/Dash/Main.java
@@ -0,0 +1,14 @@
+package com.dynocryptlabs.Dash;
+
+/**
+ * Created by rohanpanchal on 1/29/17.
+ *
+ * Main class.
+ */
+public class Main {
+
+    public static void main(String [] args)
+    {
+    }
+
+}
